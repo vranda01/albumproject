ac410b08e55646144fa60c21aedb2ed04016c028
pallawi090991 <pallawi@stylabs.in>
2019-10-24 13:30:58 +0530

    Encrpted user password before storing in database

--- routes/AlbumRouter.js
+++ routes/AlbumRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const AlbumRouter = express.Router();
+const Bcrypt = require("bcryptjs");
 const multer = require('multer');
 storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -102,44 +103,54 @@ AlbumRouter.route('/logout').get(function (req, res) {
 
 //POST ROUTES
 AlbumRouter.route('/login').post(async function (req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-  var result = loginvalidate(email, password);
-  function loginvalidate(email, password) {
-    if (email == "") {
-      return false;
+  // var email = req.body.email;
+  // var password = req.body.password;
+  // var result = loginvalidate(email, password);
+  // function loginvalidate(email, password) {
+  //   if (email == "") {
+  //     return false;
+  //   }
+  //   var emailPattern = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$");
+  //   if (!emailPattern.test(email)) {
+  //     return false;
+  //   }
+  //   if (password == "") {
+  //     return false;
+  //   }
+  //   if (password.length < 6) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  // if (result == true) {
+    var user = await User.findOne({ email: req.body.email }).exec();
+    if (!user) {
+      res.redirect('/loginerror');
     }
-    var emailPattern = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$");
-    if (!emailPattern.test(email)) {
-      return false;
+    if (!Bcrypt.compareSync(req.body.password, user.password)) {
+      res.redirect('/loginerror');
     }
-    if (password == "") {
-      return false;
-    }
-    if (password.length < 6) {
-      return false;
-    }
-    return true;
-  }
-  if (result == true) {
-    await User.findOne({ email: req.body.email, password:req.body.password}, function (err, document) {
-      console.log(document);
-      if (document) {
-        req.session.user_id = document.id;
-        req.session.fname = document.fname;
-        req.session.lname = document.lname;
-        req.session.email = document.email;
-        req.session.dob = document.dob;
-        req.session.con = document.con;
-        console.log(req.session);
-        res.redirect('/useralbum');
-      }
-      else {
-        res.redirect('/loginerror');
-      }
-    });
-  }
+     res.redirect('/useralbum');
+  //}
 });
+//     await User.findOne({ email: req.body.email, password:req.body.password}, function (err, document) {
+//       console.log(document);
+//       if (document) {
+//         req.session.user_id = document.id;
+//         req.session.fname = document.fname;
+//         req.session.lname = document.lname;
+//         req.session.email = document.email;
+//         req.session.dob = document.dob;
+//         req.session.con = document.con;
+//         console.log(req.session);
+//         res.redirect('/useralbum');
+//       }
+//       else {
+//         res.redirect('/loginerror');
+//       }
+//     });
+//   }
+// });
 
 
 AlbumRouter.route('/register').post(function (req, res) {
@@ -149,43 +160,7 @@ AlbumRouter.route('/register').post(function (req, res) {
   var password = req.body.password;
   var con = req.body.con;
   var dob = req.body.dob;
-  var result = registervalidate(fname, lname, email, password, con, dob)
-  function registervalidate(fname, lname, email, password, con, dob) {
-    if (fname == "") {
-      return false;
-    }
-    if (lname == "") {
-      return false;
-    }
-    if (email == "") {
-      return false;
-    }
-    var emailPattern = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$");
-    if (!emailPattern.test(email)) {
-      return false;
-    }
-    if (password == "") {
-      return false;
-    }
-    if (password.length < 6) {
-      return false;
-    }
-    if (dob == "") {
-      return false;
-    }
-    if (con == "") {
-      return false;
-    }
-    var conPattern = new RegExp("^(\([0-9]{3}\)\s*|[0-9]{3}\-)[0-9]{3}-[0-9]{4}$");
-    if (!conPattern.test(con)) {
-      return false;
-    }
-    if (con.length != 10) {
-      return false;
-    }
-    return true;
-  }
-  if (result == true) {
+  req.body.password = Bcrypt.hashSync(req.body.password, 10);
     const user = new User(req.body);
     console.log(user);
     user.save()
@@ -195,7 +170,6 @@ AlbumRouter.route('/register').post(function (req, res) {
       .catch(err => {
         res.status(400).send("unable to save to database");
       });
-  }
 });
 
 
@@ -206,43 +180,6 @@ AlbumRouter.route('/updateprofile').post(function (req, res) {
   var password = req.body.password;
   var con = req.body.con;
   var dob = req.body.dob;
-  var result = updatevalidate(fname, lname, email, password, con, dob)
-  function updatevalidate(fname, lname, email, password, con, dob) {
-    if (fname == "") {
-      return false;
-    }
-    if (lname == "") {
-      return false;
-    }
-    if (email == "") {
-      return false;
-    }
-    var emailPattern = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$");
-    if (!emailPattern.test(email)) {
-      return false;
-    }
-    if (password == "") {
-      return false;
-    }
-    if (password.length < 6) {
-      return false;
-    }
-    if (dob == "") {
-      return false;
-    }
-    if (con == "") {
-      return false;
-    }
-    var conPattern = new RegExp("^(\([0-9]{3}\)\s*|[0-9]{3}\-)[0-9]{3}-[0-9]{4}$");
-    if (!conPattern.test(con)) {
-       return false;
-    }
-    if (con.length != 10) {
-      return false;
-    }
-    return true;
-  }
-  if (result == true) {
   User.findOne({ email: req.body.email }, function (err, document) {
     console.log(document.id);
     sess._id = document.id;
@@ -265,7 +202,6 @@ AlbumRouter.route('/updateprofile').post(function (req, res) {
       }
     });
   });
-}
 });
 
 
